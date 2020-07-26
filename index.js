33cb20ed2901893efcfd61585c930d6f0af0b8fa
AhmedEhab1123 <59376578+AhmedEhab1123@users.noreply.github.com>
2020-07-26 04:25:10 +0200

    PulsarGenn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js")
 const fs = require('fs')
 const alt = new Discord.Client();
-var prefix = "!"
 
 alt.on("ready", () => {
   console.log(`${alt.user.tag} Is Ready!`)
@@ -17,12 +16,11 @@ alt.on("message", async message =>{
                 })
     }
   }
-})
+});
 
 alt.on("message", async message =>{
-  var channel = alt.guilds.get("736697496646058084").channels.get("736713431411064902")
   if (message.channel.type == "dm" && message.author.id == "722274179793289256"){
-    await channel.send(`<${message.content}>`)
+    await alt.guilds.get("736697496646058084").channels.get("736713431411064902").send(`<${message.content}>`)
     alt.guilds.get("736697496646058084").channels.get("736733406750310451").send("**Pulsar Generated Code Sent To <#736713431411064902>\n\nThis Is An Automated Message\n\n☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰**")
     console.log(`I Sent => ${message.content} <= To => ${alt.guilds.get("736697496646058084").name} <=`)
     function log() {
@@ -39,7 +37,7 @@ alt.on("message", async message =>{
 var cmd = require("node-cmd")
 
 alt.on("message", message => {
-  if (message.content.startsWith(prefix + "refresh") && message.member.hasPermission("ADMINISTRATOR")){
+  if (message.content.startsWith("!refresh") && message.member.hasPermission("ADMINISTRATOR")){
     cmd.run("refresh")
     message.channel.send("Script Refreshed !")
   }
